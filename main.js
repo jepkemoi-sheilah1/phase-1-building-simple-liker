0e1ac473dc4ef4c4b2d3ad61f4c1cd21e94b7a31
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-21 08:35:09 +0300

    building a simple liker

--- main.js
+++ main.js
@@ -3,9 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+//add an event listener to each heart
+document.addEventListener('DOMContentLoaded', function() {
+  const hearts = document.querySelectorAll('.like-glyph')
+  hearts.forEach(heart => {
+    heart.addEventListener('click', like)  })
+})
+.catch(error => {
+  const errorModal = document.getElementById('modal')
+  errorModal.classList.remove('hidden')
+  const errorMessage = document.getElementById('modal-message')
+  errorMessage.innerText = error
+  setTimeout(() => {
+    errorModal.classList.add('hidden')
+  }, 3000)
+  .then(() => {
+    const hearts = document.querySelectorAll('.like-glyph')
+    hearts.forEach(heart => {
+      heart.addEventListener('click', like)
+    })
+  })
+})
+addtoggleLike(heart)
+//add toggleLike function
+function toggleLike(heart) {
+  if (heart.innerText === EMPTY_HEART) {
+    heart.innerText = FULL_HEART
+    heart.classList.add('activated-heart')
+  } else {
+    heart.innerText = EMPTY_HEART
+    heart.classList.remove('activated-heart')
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
